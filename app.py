fe44b7aced2df25d2b420ff2036e099e1a4aaf0b
Emmanuel Tar <emmanueltar14@gmail.com>
2025-02-18 16:16:41 +0100

    Adding of Date and Time of Creation

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -14,6 +15,12 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     priority = db.Column(db.String(10), nullable=False, default="Low")
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)  # Auto timestamp
+
+
+    def __repr__(self):
+        return "<Task %r>" % self.id
+    
 
 
 # Create database
